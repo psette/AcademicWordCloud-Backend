da34e17ff25434887789b0ed2b372688b4c06ddf
Riley Testut <riley@rileytestut.com>
2017-02-23 23:57:13 -0800

    Adds parsers

---
+++ app/Parsers/LyricsParser.php
@@ -0,0 +1,41 @@
+<?php
+
+include('Parser.php');
+
+class LyricParser implements Parser
+{
+    /**
+     * Parses the JSON and returns Lyrics.
+     *
+     * @param array $json A JSON array of Lyrics.
+     *
+     * @return array Returns an array of Lyrics from $json.
+     */
+    function parseObject($json)
+    {
+        // To be implemented
+    }
+
+    /**
+     * Serializes array of Lyrics to JSON.
+     *
+     * @param array $lyrics An array of Lyrics to be serialized.
+     *
+     * @return array Returns the JSON representation of the LyricStorage.
+     */
+    function serializeObject($lyrics)
+    {
+        $json = Array();
+
+        foreach ($lyrics as $key)
+        {
+            $lyric = $lyrics[$key];
+
+            array_push($json, array(
+                "word" => $lyric->stringValue,
+                "frequency" => $lyric->frequency,
+            ));
+        }
+        return $json;
+    }
+}
---
+++ app/Parsers/TrackParser.php
@@ -0,0 +1,42 @@
+<?php
+
+include('Parser.php');
+include('Track.php');
+
+class TrackParser implements Parser
+{
+    /**
+     * Parses the JSON and returns a Track.
+     *
+     * @param string $json The JSON representation of a Track.
+     *
+     * @return Track Returns a Track populated with data from $json.
+     */
+    function parseObject($json)
+    {
+        $track = new Track();
+        $track->name = $json["track_name"];
+        $track->identifier = $json["track_id"];
+        return $track;
+    }
+
+    /**
+     * Serializes a Track to JSON.
+     *
+     * @param Track $track The Track to be serialized.
+     *
+     * @return array Returns the JSON representation of the Track.
+     */
+    function serializeObject($track)
+    {
+        $lyricsParser = new LyricsParser();
+
+        $json = array(
+             "name" => $track->name,
+             "identifier" => $track->identifier,
+             "frequentLyrics" => $lyricsParser.serializeObject($track->frequentLyrics),
+        );
+        return $json;
+    }
+}
+?>
