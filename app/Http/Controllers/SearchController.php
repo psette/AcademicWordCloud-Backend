133ef97727c8e938c1e8dd2013fcbeda7d909759
Tomas Craig <tcraig@usc.edu>
2017-02-27 19:51:24 -0800

    Revert "Add job for fetching lyrics"

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 include_once dirname(__FILE__) . '/../../Model/Track.php';
 include_once dirname(__FILE__) . '/../../Model/Artist.php';
 include_once dirname(__FILE__) . '/../../Model/Lyric.php';
@@ -11,12 +12,6 @@ include_once dirname(__FILE__) . '/../../Parsers/LyricParser.php';
 
 include_once dirname(__FILE__) . '/Controller.php';
 
-include_once dirname(__FILE__) . '/../../Jobs/FetchLyrics.php';
-include_once dirname(__FILE__) . '/../../Jobs/FetchTrack.php';
-
-
-use App\Jobs\FetchLyrics;
-use App\Jobs\FetchTrack;
 use Illuminate\Http\Request;
 
 use \Artist as Artist;
@@ -30,7 +25,7 @@ use \LyricParser as LyricParser;
 class SearchController extends Controller
 {
     /*
-     *  Search artists
+     *  Search artist
      *
      * @param Request $request
      * @param string $artist
@@ -38,9 +33,37 @@ class SearchController extends Controller
      * @return Artist
      *
      */
-     public function searchArtists(Request $request, $artist)
+    // public  function fetchTracks($artist ,$artistsArray, $callback = 'fetchTracksCallBack')
+    // {
+
+    //     $tracks[] =  new \Track();
+    //     $parser = new \TrackParser();
+
+    //     foreach( $artistsArray['songs'] as $trackItem ) {
+    //         if(!array_key_exists("url", $trackItem)){
+    //             echo "URL Does not exsit for " . $trackItem['name'];
+    //             continue;
+    //         }
+    //         $track = $parser->parseObject($trackItem);
+    //         $track->artist = $artist;
+    //         array_push($tracks, $track);
+    //         $this->fetchLyrics($track);
+    //     }
+
+    //     return;
+    // }
+    public function testMorphy(Request $request, $word)
     {
+         $lyricParser = new \LyricParser();
+         $stemmedWords = $lyricParser->stemWords($word);
+         var_dump($stemmedWords);
+    }
+
+    public function searchArtists(Request $request, $artist)
+    {
+
         $artists = [];
+
         $location = "http://lyrics.wikia.com/wikia.php?controller=LyricsApi&method=searchArtist&query=" . urlencode($artist);
 
         $file = @file_get_contents(html_entity_decode($location));
@@ -48,19 +71,25 @@ class SearchController extends Controller
         {
             return $artists;
         }
+
         $json = json_decode($file, true);
+
         $artistParser = new ArtistParser();
         $trackParser = new TrackParser();
         $lyricParser = new LyricParser();
+
         if (!array_key_exists("result", $json))
         {
             $response = json_encode($artists);
             return $response;
         }
+
         $artistCount = 0;
+
         foreach ($json["result"] as $artistJSON)
         {
             $artist = $artistParser->parseObject($artistJSON);
+
             if (array_key_exists("songs", $artistJSON))
             {
                 // Ensure no more than 3 artists are returned
@@ -69,36 +98,105 @@ class SearchController extends Controller
                 {
                     break;
                 }
+
                 $array = $artistJSON["songs"];
+
                 foreach ($artistJSON["songs"] as $trackJSON)
                 {
                     // Parse limited trackJSON to obtain url value.
                     // The full track JSON will be fetched + parsed in fetchTrack() (including lyrics, which aren't present here)
                     $tempTrack = $trackParser->parseObject($trackJSON);
+
                     if (!is_null($tempTrack))
                     {
-                        $track = $this->fetchTrack($tempTrack, $artist);
+                        $track = $this->fetchTrack($tempTrack->url, $artist);
+
                         if (!is_null($track))
                         {
                             $artist->tracks->attach($track);
                         }
                     }
                 }
+
+                $artist->frequentLyrics = Track::frequentLyricsFromTracks($artist->tracks);
+
                 array_push($artists, $artist);
             }
         }
+
         $serialized = array_map([$artistParser, "serializeObject"], $artists);
+
         $encoded = json_encode($serialized);
+
         return $encoded;
     }
 
-    /*
-     * Create a job for fetchig the tracks
-     * 
-     */
-    private function fetchTrack($trackURL, $trackArtist)
+    private function fetchTrack($url, $artist)
     {
-        $this->dispatch(new FetchTrack($trackURL, $trackArtist));
+        $file = @file_get_contents($url);
+        if ($file == FALSE)
+        {
+            return null;
+        }
+
+        $json = json_decode($file, true);
+
+        if (!array_key_exists("result", $json))
+        {
+            return null;
+        }
+
+        $trackJSON = $json["result"];
+
+        $trackParser = new TrackParser();
+        $trackParser->artist = $artist;
+
+        $track = $trackParser->parseObject($trackJSON);
+
+        return $track;
     }
+
+
+
+    // function fetchTracksCallBack($artist , $tracks)
+    // {
+
+    //     $lyricsDictionary = Array();
+    //     $artist->tracks = $tracks;
+
+    //     foreach($tracks as $track){
+
+    //         if( !is_null($tracks->$lyrics) ){
+
+    //         }
+
+    //     }
+
+    // }
+    //TODO FINISH LYRIC FETCHING
+    // function fetchLyrics($track, $callback = 'fetchLyricsCallBack')
+    // {
+    //     $location = $track->url;
+    //     $file = @file_get_contents(html_entity_decode($location));
+
+    //     if($file == FALSE ){
+    //         echo " File does not exsist for $track->name \n.";
+    //         return;
+    //     }
+
+    //     $json = json_decode($file, true);
+
+
+    //     $raw_lyrics  = $json["result"]["lyrics"];
+    //     //HERE ARE THE LYRICS,
+    //     return $json;
+    // }
+
+    // function fetchLyricsCallBack($track, $lyrics)
+    // {
+    //     $track->lyrics = $lyrics;
+    // }
+
 }
+
 ?>
