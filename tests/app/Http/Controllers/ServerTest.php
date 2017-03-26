f66fb9b9b10d50768548d770877f99cf813e06e7
Soren Craig <tcraig@usc.edu>
2017-03-25 18:31:53 -0700

    Add tests first

--- tests/app/Http/Controllers/ServerTest.php
+++ tests/app/Http/Controllers/ServerTest.php
@@ -2,119 +2,55 @@
 
 class ServerTest extends TestCase
 {
-	/*
-     * Test that Server::SearchArtist retrieves the file contents from the given artist
-     * Input: $dummyRequest - Illuminate\Http\Request Object
-     *        $testArtist - String of artist name you would like to search
-     * Description: This function is testing that the searchArtist() function retrieves the
-     * string of the corresponding artist search by invoking he method getArtistPageString()
-     * with the arguments of the artist
-     *
+    /*
+     * Test IEEE file retrieval
      */
-	public function testSearchArtistRetrievesFileContents()
+    public function testGetIEEEFile()
     {
-    	$dummyRequest = new Illuminate\Http\Request();
-        $testArtist = "the+killers";
+        $testAuthor = "Sette";
 
         // Create a stub for the Server class.
         $serverStub = $this->createMock(App\Http\Controllers\Server::class);
+
         $serverStub = $this->getMockBuilder(App\Http\Controllers\Server::class)
-                         ->setMethods(['getArtistPageString'])
+                         ->setMethods(['get_IEEE_file'])
                          ->getMock();
 
-        // ensure that it gets the file contents from the given artist
+        // ensure that it gets the file contents from the given author
         $serverStub->expects($this->once())
-                 ->method('getArtistPageString')
-                 ->with($this->equalTo($testArtist));
-        // invoke the function
-       $serverStub->searchArtists($dummyRequest, $testArtist);
+                 ->method('get_IEEE_file')
+                 ->with($this->equalTo($testAuthor));
 
+        // invoke the function
+       $serverStub->get_IEEE_file($testAuthor);
     }
 
     /*
-     * Test that Server::SearchArtist returns an empty array if no lyrics are found
-     * Input: $dummyRequest - Illuminate\Http\Request Object
-     *        $testArtist - String of artist name you would like to search
-     * Description: This function is testing that the searchArtist() function returns
-     *              an empty array, as expected by the client, if there is no array with the
-     *              key of "result". This may occur if the api receives a search string that
-     *              it does not have any results for
-     *
-     *
+     * Test the parseXMLObject()
      */
-    public function testEmptyArrayReturned()
+    public function testParseXMLObject()
     {
-        $dummyRequest = new Illuminate\Http\Request();
-        $testArtist = "foo+bar";
 
+        $testAuthor = "foo+author";
+        // Create a stub for the Server class.
         // Create a stub for the Server class.
         $serverStub = $this->createMock(App\Http\Controllers\Server::class);
+
         $serverStub = $this->getMockBuilder(App\Http\Controllers\Server::class)
-                         ->setMethods(['getArtistPageString'])
+                         ->setMethods(['parseXMLObject'])
                          ->getMock();
 
-        // ensure that it gets the file contents from the given artist
-        // Configure the stub.
-        $serverStub->method('getArtistPageString')
-             ->willReturn(false);
-        // invoke the function
-       $response = $serverStub->searchArtists($dummyRequest, $testArtist);
+        $testArray = array();
+        /// invoke the function
+        $response = $serverStub->parseXMLObject($testArray);
 
-       $this->assertEquals($response, array());
+        $this->assertEquals($response, NULL);
     }
 
     /*
-     * Test that Server::SearchArtist returns an empty array if no lyrics are found
-     * Input: $dummyRequest - Illuminate\Http\Request Object
-     *        $testArtist - String of artist name you would like to search
-     * Description: This function is testing that the searchArtist() function returns
-     *              an empty array, as expected by the client, if there is no array with the
-     *              key of "songs" within the array of "results. This may occur if the api receives a search string that
-     *              it does not have any lyrics for
-     *
      *
      */
-    public function testNoSongsFound()
-    {
-        $jsonReponse = json_encode(array(
-            "foo" => "bar"
-        ));
-
-        $dummyRequest = new Illuminate\Http\Request();
-        $testArtist = "the+killers";
-
-        // Create a stub for the Server class.
-        $serverStub = $this->createMock(App\Http\Controllers\Server::class);
-        $serverStub = $this->getMockBuilder(App\Http\Controllers\Server::class)
-                         ->setMethods(['getArtistPageString'])
-                         ->getMock();
-
-        // ensure that it gets the file contents from the given artist
-        // Configure the stub.
-        $serverStub->method('getArtistPageString')
-             ->willReturn($jsonReponse);
-        // invoke the function
-       $response = $serverStub->searchArtists($dummyRequest, $testArtist);
-
-       $decodedResponse = json_decode($response);
-
-       $this->assertEquals($decodedResponse, array());
-    }
-
-
-    /*
-     * Test that Server::SearchArtist returns an empty array if no lyrics are found
-     * Input: $dummyRequest - Illuminate\Http\Request Object
-     *        $testArtist - String of artist name you would like to search
-     * Description: This function is testing that the searchArtist() function returns
-     *              the json response in the format that the client expects to consume it in.
-     *              This includes ensuring that the lyrics are nested properly within the "result"
-     *              array with the corresponding lyrics to songs listed in an array under the key "songs"
-     *              which then contains the corresponding lyrics
-     *              This sample response was created by constructing the response given the barebones JSON structure
-     *              expected, and then populating that array with lyrics that were copy and pasted from an online search.
-     */
-    public function testValidSongsResponse()
+    public function testSearchAuthors()
     {
         /*
          * Load an expected response that we define as expected
@@ -122,24 +58,18 @@ class ServerTest extends TestCase
         $jsonResponse = file_get_contents("SampleResponse.json", true);
         $expectedResponse = json_decode(file_get_contents("ExpectedBody.json", true));
         $dummyRequest = new Illuminate\Http\Request();
-        $testArtist = "the+killers";
+        $testAuthor = "sette";
 
         // Create a stub for the Server class.
         $serverStub = $this->createMock(App\Http\Controllers\Server::class);
         $serverStub = $this->getMockBuilder(App\Http\Controllers\Server::class)
-                         ->setMethods(['getArtistPageString'])
+                         ->setMethods(['searchAuthors'])
                          ->getMock();
 
-        // return the sample JSON response when the search is made for it
-        $serverStub->method('getArtistPageString')
-             ->willReturn($jsonResponse);
-
         // invoke the function and retrieve the response
-       $response = $serverStub->searchArtists($dummyRequest, $testArtist);
-       $decodedContent = json_decode($response->content());
-
+       $response = $serverStub->searchAuthors($dummyRequest, $testAuthor);
+       $decodedContent = json_decode($response);
        // assert that the response matches the calculated expected response from the given crafted input
-       $this->assertEquals($expectedResponse, $decodedContent);
-       $this->assertEquals(200, $response->status());
+       $this->assertTrue(is_string($testAuthor));
     }
 }
