200477b1331b4e2ed30e5ef5b892a040519c3589
Michelle <yinm@usc.edu>
2017-03-28 21:12:19 -0700

    wrote preliminary function to parse IEEE DOM for the actual link to the paper's pdf

--- app/Parsers/PDFParser.php
+++ app/Parsers/PDFParser.php
@@ -4,16 +4,49 @@ include_once dirname(__FILE__) . '/../../vendor/autoload.php';
 
 class PDFParser
 {
+    /**
+     * Extracts text from a PDF.
+     *
+     * @param PDF $Pdf The PDF to be extracted.
+     *
+     * @return array Returns the text representation of the PDF.
+     */
     public static function getTextFromPDF($pdf)
     {
         // Parse pdf file and build necessary objects.
         $parser = new \Smalot\PdfParser\Parser();
-        $pdf = $parser->parseFile('https://www.acm.org/membership/NIC.pdf');
 
-        //NOTE: Not sure why IEEE PDFS are all returning errors
-        //$pdf = $parser->parseFile('http://ieeexplore.ieee.org/ielx5/4130641/4130642/04130757.pdf');
-
+        $pdf = $parser->parseFile($pdf);
         $text = $pdf->getText();
-        echo $text;
+
+        return $text;
+    }
+
+    /**
+     * Scrapes the DOM of IEEE pdf url to get the actual pdf link.
+     *
+     * @param Link $Link The pdf link given by the IEEE .
+     *
+     * @return array Returns the String representation of the link.
+     */
+    public static function getPDFLinkFromIEEE($link)
+    {
+
+        //$link = $link . '&tag=1'
+        $page = file_get_contents($link);
+        //echo htmlentities($page);
+
+        //locate the embed html by separating the html string into two parts
+        $locateEmbedArray = explode('<embed id="plugin', $page);
+
+        //locate the beginning string of the src of the pdf
+        $srcArray = explode('src="', $locateEmbedArray[1]);
+
+        //srcArray[1] should contain http://ieeexplore.ieee.org/ielx2/597/5570/00213554.pdf?tp=&arnumber=213554&isnumber=5570" plus more
+
+        $src = explode('"', $srcArray[1]);
+
+        return $src[0];
+        echo "hello <br>";
     }
 }
