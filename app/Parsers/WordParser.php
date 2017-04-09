0a575edcf1558de35261d540e0aa4f6ec4134200
Riley Testut <riley@rileytestut.com>
2017-04-08 17:02:21 -0700

    Consolidates ACMWordPapers and WordParser logic into WordParser

--- app/Parsers/WordParser.php
+++ app/Parsers/WordParser.php
@@ -1,42 +1,75 @@
 <?php
-include_once dirname(__FILE__) . '/../Model/Word.php';
 
-class WordParser {
+include_once __DIR__  . '/Parser.php';
+include_once __DIR__  . '/../Model/Word.php';
 
-    function cmp($a, $b){
-            return $a->frequency <= $b->frequency;
-    }
+/**
+ * Parser to parse Word objects.
+ */
+class WordParser implements Parser
+{
+    /**
+    * The Papers the word to be parsed belong to.
+    *
+    * @var ModelSet of Papers
+    */
+    var $papers;
 
-    function parseWord($paperText,$paperName ){
+    /**
+     * Parses the words string and returns Words.
+     *
+     * @param string $words The words in a Paper.
+     *
+     * @return array Returns an array of Words from $json.
+     */
+    function parseObject($string)
+    {
+        // Split $words into words separated by non-letters (except ' and -).
+        $string = preg_replace('/[^a-z0-9]+/i', ' ', $string);
+        $terms = explode(" ", $string);
 
-        $paperText = preg_replace("/[^A-Za-z0-9 ]/", ' ', strtolower($paperText) );
-
-        $wordFrequencyInPaper = array_count_values(str_word_count($paperText, 1)) ;
-        arsort( $wordFrequencyInPaper );
         $words = [];
-        foreach($wordFrequencyInPaper as $nugget => $value){
-            $word = new Word();
-            $word->stringValue= $nugget;
-            $word->frequency = $value;
-            array_push($word->papers, $paperName);
-            array_push($words, $word);
+        foreach ($terms as $term)
+        {
+            $term = strtolower($term);
+
+            $word = null;
+            if (array_key_exists($term, $words))
+            {
+                $word = $words[$term];
+            }
+            else
+            {
+                $word = new Word();
+                $word->stringValue = $term;
+                $word->identifier = $term;
+                if (!is_null($this->papers))
+                {
+                    $word->papers = $this->papers;
+                }
+            }
+            $word->frequency = $word->frequency + 1;
+            $words[$term] = $word;
         }
-        usort($words, array($this, "cmp"));
+        usort($words, ["Word", "compareByFrequency"]);
+
         return $words;
     }
-     /*
-     * Serializes a word to JSON.
+
+    /**
+     * Serializes a Word to JSON.
      *
-     * @param word $lyric Lyric to be serialized.
+     * @param Word $word Word to be serialized.
      *
-     * @return array Returns the JSON representation of the word.
+     * @return array Returns the JSON representation of the Word.
      */
-    function serializeObject($word){
+    function serializeObject($word)
+    {
         $json = [
              "stringValue" => $word->stringValue,
              "frequency" => $word->frequency,
-             "paper" => $word->papers
         ];
         return $json;
     }
 }
+?>
